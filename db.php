fbbfe91195f53f18168fca094c2837895b762a7e
yash128 <75480324+yash128@users.noreply.github.com>
2025-01-27 23:23:17 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect('localhost:3306', 'peakmein_peakme', 'Goyals%^&56789');
-mysqli_select_db($con, 'peakmein_somethingnew');
+$con = mysqli_connect('localhost:3306', 'host', 'password');
+mysqli_select_db($con, 'db_name');
 ?>
